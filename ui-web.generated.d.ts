4d657e343a000be08c84edf39de1cf7126ffca31
zccz14 <zccz14@users.noreply.github.com>
2025-09-13 14:05:44 +0000

    deploy: No-Trade-No-Life/Yuan@3cee2a063dd545fc5a9eb1a09afc3b4f3254c896

--- ui-web.generated.d.ts
+++ ui-web.generated.d.ts
@@ -552,8 +552,86 @@ type index_d$5_IAssociationRule = IAssociationRule;
   export { index_d$4_InlineTerminalId as InlineTerminalId, index_d$4_hostUrl$ as hostUrl$, index_d$4_isTerminalConnected$ as isTerminalConnected$, index_d$4_terminal$ as terminal$, index_d$4_useTerminal as useTerminal, index_d$4_useTick as useTick };
 }
 
+/**
+ * @public
+ */
+interface ITradeCopierTradeConfig {
+    id?: string;
+    account_id: string;
+    product_id: string;
+    max_volume_per_order: number;
+    limit_order_control?: boolean;
+    disabled?: boolean;
+}
+/**
+ * @public
+ */
+interface ITradeCopyRelation {
+    id?: string;
+    source_account_id: string;
+    source_product_id: string;
+    target_account_id: string;
+    target_product_id: string;
+    multiple: number;
+    /** 根据正则表达式匹配头寸的备注 (黑名单) */
+    exclusive_comment_pattern?: string;
+    disabled?: boolean;
+}
+ type ITradeCopierStrategyBase = {
+    /**
+     * 策略类型
+     */
+    type?: string;
+    /**
+     * 最大订单量限制
+     */
+    max_volume?: number;
+};
+ type ITradeCopierStrategyConfig = {
+    /**
+     * 全局默认配置
+     */
+    global?: ITradeCopierStrategyBase;
+    /**
+     * 按照 product 特殊覆盖的配置
+     */
+    product_overrides?: Record<string, ITradeCopierStrategyBase>;
+};
+/**
+ * @public
+ */
+interface ITradeCopierConfig {
+    /**
+     * 实际账户 ID
+     *
+     * - (强制) 预期账户 ID 格式: `TradeCopier/Expected/${account_id}`
+     * - (建议) 预览账户 ID 格式: `TradeCopier/Preview/${account_id}`
+     *
+     * (防呆设计) 建议在使用时，先配置预览账户，确认无误后，再发布配置到预期账户。直接修改预期账户的配置，可能会导致跟单出现意外的问题。
+     */
+    account_id: string;
+    /**
+     * 是否启用跟单
+     */
+    enabled: boolean;
+    /**
+     * 跟单策略配置
+     */
+    strategy: ITradeCopierStrategyConfig;
+    created_at: string;
+    updated_at: string;
+}
+
+ const schemaOfTradeCopierConfig: JSONSchema7;
+
+type index_d$3_ITradeCopierConfig = ITradeCopierConfig;
+type index_d$3_ITradeCopierStrategyBase = ITradeCopierStrategyBase;
+type index_d$3_ITradeCopierStrategyConfig = ITradeCopierStrategyConfig;
+type index_d$3_ITradeCopierTradeConfig = ITradeCopierTradeConfig;
+type index_d$3_ITradeCopyRelation = ITradeCopyRelation;
+ const index_d$3_schemaOfTradeCopierConfig: typeof schemaOfTradeCopierConfig;
  namespace index_d$3 {
-  export {  };
+  export { type index_d$3_ITradeCopierConfig as ITradeCopierConfig, type index_d$3_ITradeCopierStrategyBase as ITradeCopierStrategyBase, type index_d$3_ITradeCopierStrategyConfig as ITradeCopierStrategyConfig, type index_d$3_ITradeCopierTradeConfig as ITradeCopierTradeConfig, type index_d$3_ITradeCopyRelation as ITradeCopyRelation, index_d$3_schemaOfTradeCopierConfig as schemaOfTradeCopierConfig };
 }
 
  namespace index_d$2 {
